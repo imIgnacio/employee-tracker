71ac1e86a88efb56d210b818cb567bb843026316
Ignacio Diaz <nachodiaz99@gmail.com>
2021-11-13 10:12:14 +1100

    Dependencies installed

--- server.js
+++ server.js
@@ -1,3 +1,26 @@
 const mysql = require('mysql2');
 const inquirer = require('inquirer');
 const cTable = require('console.table');
+
+function init() {
+    console.log("Welcome to Employee Tracker V1.0.0");
+    
+    inquirer
+    .prompt({
+        type: 'list',
+        message: 'Please select one of the following choices',
+        name: 'choice',
+        choices: [
+            'View all departments',
+            'View all roles',
+            'View all employees',
+            'Add a department',
+            'Add a role',
+            'Add an employee',
+            'Update an employee role'
+        ]
+    })
+    .then((data) => {
+         runFunction(data);
+    })
+}
